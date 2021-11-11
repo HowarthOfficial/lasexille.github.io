fdaa2a43c0882ced8c3940289d01d8f7909026ca
Badcoder <47592466+badlycoder@users.noreply.github.com>
2021-11-11 20:24:13 +0300

    1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lasexille-page
+ 
---
+++ js/config.js
@@ -0,0 +1,116 @@
+
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "images/logo.png";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = true;
+
+/*Video background
+Avaiable options:
+"backgrounds/videos/aurora.webm"
+"backgrounds/videos/bluefall.webm"
+"backgrounds/videos/bluetilefloor.webm"
+"backgrounds/videos/bluewhitecircles.webm"
+"backgrounds/videos/bokehlens.webm"
+"backgrounds/videos/cleanbokeh.webm"
+"backgrounds/videos/coldnight.webm"
+"backgrounds/videos/colorfulburst.webm"
+"backgrounds/videos/dancingstars.webm"
+"backgrounds/videos/dropsflares.webm"
+"backgrounds/videos/focusorangebokeh.webm"
+"backgrounds/videos/glimmering.webm"
+"backgrounds/videos/glowingrings.webm"
+"backgrounds/videos/greencircles.webm"
+"backgrounds/videos/hues.webm"
+"backgrounds/videos/inthedeep.webm"
+"backgrounds/videos/modernflourish.webm"
+"backgrounds/videos/orangeaura.webm"
+"backgrounds/videos/warminside.webm"
+"backgrounds/videos/yellowgreen.webm"
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format */
+var l_background = "backgrounds/videos/glimmering.webm";
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+	"backgrounds/images/1.png",
+	"backgrounds/images/2.png",
+	"backgrounds/images/3.png",
+	"backgrounds/images/4.png"
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = true;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+	{ogg: "songs/Lullaby of Deserted Hell.ogg", name: "Lullaby of Deserted Hell"},
+	{ogg: "songs/Nominal - Everyday Anyone.ogg", name: "Nominal - Everyday Anyone"},
+	{ogg: "songs/Skeler - Geneva.ogg", name: "Skeler - Geneva"},
+	{ogg: "songs/Tommee Profitt - In The End.ogg", name: "Tommee Profitt - In The End"},
+	{ogg: "songs/FatB - Cotton Cloud.ogg", name: "FatB - Cotton Cloud"}
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 25;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Саппорт по созданию серверов GMod/CS:GO",
+"Начинающий аниматор в Source Filmmaker",
+"Продавец скинов на заказ для CS:GO",
+"Ролевой редактор в Octothorp Team",
+"Желаешь поддержать меня? — снизу четвертая иконка "
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
